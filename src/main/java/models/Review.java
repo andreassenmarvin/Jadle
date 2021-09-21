f84d131a42985c8752cb04751fc59f281fcf503f
andreassenmarvin <marvinandreassen@gmail.com>
2021-09-21 07:50:07 +0300

    Create Review class and test

--- src/main/java/models/Review.java
+++ src/main/java/models/Review.java
@@ -1,4 +1,56 @@
 package models;
 
 public class Review {
+    private String content;
+    private String writtenBy;
+    private int rating;
+    private int id;
+    private int restaurantId; //will be used to connect Restaurant to Review (one-to-many)
+
+    public Review(String content, String writtenBy, int rating, int restaurantId) {
+        this.content = content;
+        this.writtenBy = writtenBy;
+        this.rating = rating;
+        this.restaurantId = restaurantId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getWrittenBy() {
+        return writtenBy;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setWrittenBy(String writtenBy) {
+        this.writtenBy = writtenBy;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
 }
